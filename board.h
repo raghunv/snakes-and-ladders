2ead84156f99e71954d1f8bac259d76481d36a3c
Sam Nunnery <nunnerys14@mycu.concord.edu>
2013-01-29 13:57:25 -0800

    Create board.h
    Header for the game board class

---
+++ board.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_H
+#define BOARD_h
+#include "player.h"
+
+class Board
+{
+    public:
+        void print_board();
+        void set_board();
+        void reset_board();
+        void update_board(Player& player_moving, Player& player_idle);
+
+        Board();
+
+    protected:
+        int board[100][4];
+};
+
+#endif
